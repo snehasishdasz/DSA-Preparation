4d315d1853bf67c5104fb5018249a8857ce664e6
Snehasish Das <78856467+snehasishdasz@users.noreply.github.com>
2023-11-03 20:59:43 +0530

    test

---
+++ loop.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class loop {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number: ");
+        int n = sc.nextInt();
+
+        int sum = 0;
+        for (int i = 1; i <= n; i++) { // Start the loop from 1, not 0
+            sum = sum + i;
+        }
+        System.out.println("Sum of " + n + " natural numbers is: " + sum);
+    }
+}

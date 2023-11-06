7c489f9c74b1d84d68d739415e70b36082af01e0
Snehasish Das <78856467+snehasishdasz@users.noreply.github.com>
2023-11-07 00:43:15 +0530

    pattern continue

---
+++ pattern9.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class pattern9 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number: ");
+        int n = sc.nextInt();
+
+       
+        //outer loop
+        for(int i=1;i<=n;i++)
+        {
+            //inner loop for number
+            for(int j=1;j<=i;j++)
+            {
+               System.out.print(j);
+            }
+            //for space 
+            int start = 2*(n-i);
+            for(int j=1;j<=start;j++)
+            {
+               System.out.print(" ");
+            }
+
+            //for number
+            for(int j=i;j>=1;j--)
+            {
+               System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+}
